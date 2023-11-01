b7ec87e453169ea53567f3a6d6abd122e1e7c12f
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-01 19:41:29 +0600

    added content countries file

--- js/countries.js
+++ js/countries.js
@@ -17,6 +17,9 @@ const displayCountries = countries =>{
      /* .join(' ') reason coma na thakar jonno serial akare line thakbe*/
      container.innerHTML = countriesHTML.join(' ');
 }
+
+// Jokhon kuno ekta object k parameter hisebe pash korbo, sei object k 1 ta , 2 ta property k niye kisu ekta korte chai tahole Option 1 ase , setar moto kore destructuring variable declare korte pari . Niche Total 3 ta step ase j keno ekta setp diye korle kaj korbe . //
+
 /* Option 1
 const getCountryHTML = country =>{
     // destructuring use option1 variable declare //
@@ -29,12 +32,45 @@ const getCountryHTML = country =>{
     `
 }*/
 
-/* Original */
+/* Original 
 const getCountryHTML = country =>{
     return `
     <div class="country" >
       <h2>${country.name.common}</h2>
       <img src="${country.flags.png}">
+    </div>
+    `
+}*/
+
+/* Original ta ar ek vabe chinta kora jay
+ const getCountryHTML = country =>{
+    const {name, flags} = country;
+    return `
+    <div class="country" >
+      <h2>${name.common}</h2>
+      <img src="${flags.png}">
+    </div>
+    `
+}
+*/
+
+/* 2nd Original k ghuray pechay ar ek vabe kora jay For Example
+   // Parameter e majhe destructuring kore fela bola hoy//
+   const getCountryHTML = ({name, flags}) =>{
+    return `
+    <div class="country" >
+      <h2>${name.common}</h2>
+      <img src="${flags.png}">
+    </div>
+    `
+}
+*/
+const getCountryHTML = ({name, flags, area}) =>{
+    return `
+    <div class="country" >
+      <h2>${name.common}</h2>
+      <p>Area: ${area}</p>
+      <img src="${flags.png}">
     </div>
     `
 }
