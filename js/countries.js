2d6ce8bc256947b98f35e9f739e387aa78cd0c50
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-01 15:00:02 +0600

    countries js file content added

--- js/countries.js
+++ js/countries.js
@@ -1,15 +1,25 @@
 /* Function likhe felbo, () etar kuno parameter nai */
 const loadCountries = () =>{
+    /* fetch r moddhe j link deya ase seta "All API theke link neya hoyeche" */
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
-    .then(data => displayCountries(data) );
+    .then(data => displayCountries(data));
 }
 
 /* Countries data gulo ekhane thakbe,  ar ekta function likhbo*/
 const displayCountries = countries =>{
       console.log(countries);
       /*variable declare line, */
-      const allCountriesHTML =
+      const countriesHTML = countries.map(country => getCountryHTML(country));
+      console.log(countriesHTML);
+}
+
+const getCountryHTML = country =>{
+    return `
+    <div>
+      <h2>${country.name.common}</h2>
+    </div>
+    `
 }
 /* function call korlam */
 loadCountries();
